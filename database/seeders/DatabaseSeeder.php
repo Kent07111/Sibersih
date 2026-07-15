92179682c81e276930b408a8dfd0e1c62681e58a
kent_prayoga <kentprayoga07@gmail.com>
2026-07-15 14:43:19 +0700

    v2.1

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,7 +23,7 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             AdminSeeder::class,
-            SettingsSeeder::class
+            SettingSeeder::class
         ]);
     }
 }
